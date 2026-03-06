b537b8a691b8a71fa5da25ac8a09ed03863fd442
Siti Masito <sitimasito@saintek.unipdu.ac.id>
2026-03-06 09:13:27 +0700

    feat: tambah route perkenalan sitimasito

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return 'Halaman Utama Project Laravel';
+});
+
 Route::get('/perkenalan', function () {
 
     return '<h1>Halo! Nama saya Joened Sastra</h1>
@@ -9,4 +13,10 @@ Route::get('/perkenalan', function () {
             <p>NIM: 4124017 | Prodi: Sistem Informasi</p>
 
             <p>Saya siap belajar Laravel! 🚀</p>';
+});
+
+//aku sebagai kolaburator 
+Route::get('/sitimasito', function () {
+    return '<h1>Halo! Nama saya Sitimasito</h1>
+            <p>Kolaborator project Laravel</p>';
 });
